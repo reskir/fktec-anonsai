902635371d66db70f9531dd17db16e03ab87d854
Kiril Abaskin <kiril@1stdibs.com>
2018-04-17 22:27:36 +0300

    updated port

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.get('/',function(req,res){
   
 //pp.get('/', (req, res) => res.render('index'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'))
