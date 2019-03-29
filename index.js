efabf3780ce0f6cb43f9788a85fe9399be96132d
Kiril Abaskin <kiril@1stdibs.com>
2019-03-29 08:38:06 +0200

    biudzetas

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',function(req,res){
-    res.type('.html');
-    res.sendFile(path.join(__dirname+'/index.html'));
+app.get("/", function(req, res) {
+  res.type(".html");
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
-  
-app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 5000!'))
+
+app.get("/biudzetas", function(req, res) {
+  res.type("html");
+  res.sendfile(path.join(__dirname + "/biudzetas.html"));
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log("Example app listening on port 5000!")
+);
---
+++ public/biudzetas.js
@@ -0,0 +1,77 @@
+const pieOptions = {
+    chart: {
+        type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 50,
+        },
+    },
+    plotOptions: {
+        pie: {
+            innerSize: 100,
+            depth: 50,
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}',
+            },
+        },
+    },
+};
+
+Highcharts.chart('revenue', {
+    ...pieOptions,
+    title: {
+        text: 'TEC komandos 2017 - 2018 metų biudžetas',
+    },
+    subtitle: {
+        text: 'Išlaidos',
+        style: {
+            'font-size': '20px',
+            color: 'red',
+        },
+    },
+    colors: ['#D03B5B', '#ff0039', '#aa0026'],
+    tooltip: {
+        pointFormat: '{series.name}: <b>€ {point.y}</b>',
+    },
+    series: [
+        {
+            name: 'Išlaidos',
+            data: [
+                ['Rungtynės', 200],
+                ['Turnyrų mokesčiai', 100],
+                ['Stadionas', 300],
+            ],
+        },
+    ],
+});
+
+Highcharts.chart('expense', {
+    ...pieOptions,
+    title: {
+        text: 'TEC komandos 2017 - 2018 metų biudžetas',
+    },
+    subtitle: {
+        text: 'Pajamos',
+        style: {
+            'font-weight:': '900',
+            'font-size': '20px',
+            color: 'green',
+        },
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>€ {point.y}</b>',
+    },
+    series: [
+        {
+            name: 'Išlaidos',
+            data: [
+                ['2% Parama', 200],
+                ['Nario mokesti', 100],
+                ['Rėmėjai', 300],
+            ],
+        },
+    ],
+});
